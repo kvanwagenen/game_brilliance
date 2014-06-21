35f471c47cd1a52d468ff910dfcbe473bc8ac9fc
Kyle Van Wagenen <kvanwagenen@gmail.com>
2014-06-20 23:23:06 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameBrilliance::Application.config.secret_key_base = '8e35d45307a4bba967514bb46f4daf17468daf31b361f6651985957f86f55d654016fd4e82d2cef43dbee6b51ab11706e86944835829b4a6e1ca6c7639c0942b'
